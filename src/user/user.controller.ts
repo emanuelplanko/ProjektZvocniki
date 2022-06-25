fd0f713043d8ac2cd3560046d55fd449a1bbe6d1
LongTime2 <planko.emanuel@gmail.com>
2022-06-26 01:48:02 +0200

    CRUD za userja

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,44 @@
-import {Controller, Get} from '@nestjs/common';
+import {Body, Controller, Delete, Get, Param, Post, Put} from '@nestjs/common';
+import {UserService} from "./user.service";
+import {User} from "./user.entity";
 
 @Controller('users')
 export class UserController {
+    constructor(
+        private userService: UserService) {
+    }
 
-
-// all je funkcija
-    @Get()
+    //all je funkcija
+    //zadnji del na koncu: /
+    //pri GET je v oklepaju "users" endpoint
+    @Get('users')
     all() {
-        return 'VSS je zakon!';
+        return this.userService.all();
+    }
+
+    //select Body in POSTMAN
+    @Post('user')
+    create (@Body() data): Promise<User> {
+        return this.userService.create(data);
+    }
+
+    @Get(':id')
+    getUserById(@Param('id') id: number) :Promise<User> {
+        return this.userService.findOne({id});
+    }
+
+    //metoda za update
+    @Put(':id')
+    async update(
+        @Param('id') id:number,
+        @Body() data
+    ) : Promise<User> {
+        return await this.userService.update(id, data);
+    }
+
+    @Delete(':id')
+    delete(@Param('id') id:number): Promise<any> {
+        return this.userService.delete(id);
     }
 
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,40 @@
 import { Injectable } from '@nestjs/common';
+import {InjectRepository} from "@nestjs/typeorm";
+import {User} from "./user.entity";
+import {Repository} from "typeorm";
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(
+        @InjectRepository(User) private readonly userRepository: Repository<User>
+    ) {
+    }
+
+    //povezava na repositorij od baze
+    async all(): Promise<User[]> {
+        return  this.userRepository.find();
+    }
+
+    //pošlje podatke o novo ustvarjenem userju
+    create(data): Promise<User> {
+        return this.userRepository.save(data);
+    }
+
+    //vrača userja
+    findOne(condition): Promise<User> {
+        return this.userRepository.findOne(condition);
+    }
+
+    //posodobitev podatkov
+    //await-dokler se to ne izvrši, ne iti na naslednji ukaz
+    async update(id, data): Promise<User> {
+        await this.userRepository.update(id, data);
+        return this.findOne({id});
+    }
+
+    //id je tipa number
+    delete(id: number): Promise<any> {
+        return this.userRepository.delete(id);
+    }
+
+}
